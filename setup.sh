46a19afb6bfce50d4f20b6bf559dc391af199b30
dxpk_uk <up747816@myport.ac.uk>
2018-09-19 12:15:06 +0100

    one setup script for crontab and timelapse

--- setup.sh
+++ setup.sh
@@ -7,6 +7,25 @@ read endtime
 echo Enter START DAY: Allowed values: MON TUE WED THU FRI SAT SUN
 read startday
 echo Enter END DAY: Allowed values: MON TUE WED THU FRI SAT SUN
-echo endday
+read endday
+echo Enter location
+read location
 
+cat << EOF > /home/pi/rpi-timelapse/camera.sh
+#!/bin/bash
+LOCATION = $loction
+FOLDER = $(date + "%Y-%m-%d")
+DATE = $(date + "%Y-%m-%d_%H%M")
+raspistill -vf -hf -o /home/pi/$FOLDER/$LOCATION_$DATE.jpg
+EOF
+
+cat << EOF > /home/pi/rpi-timelapse/generate-folder.sh
+#!/bin/bash
+DATE = $(date + "%Y-%m-%d")
+mkdir /home/pi/$DATE
+EOF
+
+(crontab -l 2>/dev/null; echo "00**$startday-$endday /home/pi/rpi-timelapse/generate-folder.sh 2>&1")
 (crontab -l 2>/dev/null; echo "*$starttime-$endtime**$startday-$endday /home/pi/rpi-timelapse/camera.sh 2>&1")
+
+reboot
