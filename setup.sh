bc1cc75289d20a11b884588518aaeeeaac8d4748
dxpk_uk <up747816@myport.ac.uk>
2018-09-19 12:36:01 +0100

    neatened prompts. Added user feedback. added rpi update'

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-crontab -r
-echo Enter START TIME: Allowed values: 0 - 23
-read starttime
-echo Enter END TIME: Allowed values: 0 - 23
-read endtime
-echo Enter START DAY: Allowed values: MON TUE WED THU FRI SAT SUN
-read startday
-echo Enter END DAY: Allowed values: MON TUE WED THU FRI SAT SUN
-read endday
-echo Enter location
-read location
+
+sudo apt-get update
+
+sudo crontab -r
+read -p 'Enter START TIME: Allowed values: 0 - 23: ' starttime
+read -p 'Enter END TIME: Allowed values: 0 - 23: ' endtime
+read 'Enter START DAY: Allowed values: MON TUE WED THU FRI SAT SUN: ' startday
+read 'Enter END DAY: Allowed values: MON TUE WED THU FRI SAT SUN: ' endday
+read 'Enter location: ' location
 
 cat << EOF > /home/pi/rpi-timelapse/camera.sh
 #!/bin/bash
@@ -25,7 +23,14 @@ DATE = $(date + "%Y-%m-%d")
 mkdir /home/pi/$DATE
 EOF
 
+sudo chmod +x /home/pi/rpi-timelapse/camera.sh
+sudo chmod +x /home/pi/rpi-timelapse/generate-folder.sh
+
+echo Creating crontab
+
 (crontab -l 2>/dev/null; echo "00**$startday-$endday /home/pi/rpi-timelapse/generate-folder.sh 2>&1")
 (crontab -l 2>/dev/null; echo "*$starttime-$endtime**$startday-$endday /home/pi/rpi-timelapse/camera.sh 2>&1")
+
+echo RPi will now reboot...
 
 reboot
